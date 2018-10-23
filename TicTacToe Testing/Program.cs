d1b98091ff373b32f24db4b4caa00427acd56f69
Art Weiss <tuf40221@temple.edu>
2018-10-23 18:28:44 -0400

    added a whole new program because I can't follow instructions

--- TicTacToe Testing/Program.cs
+++ TicTacToe Testing/Program.cs
@@ -8,7 +8,7 @@ namespace TicTacToe_Testing
 {
     class Program
     {
-        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static char[] boardSquares = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         static int player = 1;
         static int choice;
         static int winFlag = 0;
@@ -37,18 +37,18 @@ namespace TicTacToe_Testing
                 {
                     if (player % 2 == 0)
                     {
-                        arr[choice] = 'O';
+                        boardSquares[choice] = 'O';
                         player++;
                     }
                     else
                     {
-                        arr[choice] = 'X';
+                        boardSquares[choice] = 'X';
                         player++;
                     }
                 }
                 else
                 {
-                    Console.WriteLine("The row {0} is already marked with {1}", choice, arr[choice]);
+                    Console.WriteLine("The row {0} is already marked with {1}", choice, boardSquares[choice]);
                     Console.WriteLine("\n");
                     Console.WriteLine("Wait, board is loading again.....");
                     Thread.Sleep(2000);
@@ -75,19 +75,19 @@ namespace TicTacToe_Testing
         public static void CreateBoard()
         {
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", boardSquares[1], boardSquares[2], boardSquares[3]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", boardSquares[4], boardSquares[5], boardSquares[6]);
             Console.WriteLine("_____|_____|_____ ");
             Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("  {0}  |  {1}  |  {2}", boardSquares[7], boardSquares[8], boardSquares[9]);
             Console.WriteLine("     |     |      ");
         }
 
         public static bool SpaceInUse()
         {
-            if (arr[choice] != 'X' && arr[choice] != 'O')
+            if (boardSquares[choice] != 'X' && boardSquares[choice] != 'O')
             {
                 return false;
             }
@@ -100,45 +100,45 @@ namespace TicTacToe_Testing
         private static int CheckWin()
         {
             //Winning Condition For First Row   
-            if (arr[1] == arr[2] && arr[2] == arr[3])
+            if (boardSquares[1] == boardSquares[2] && boardSquares[2] == boardSquares[3])
             {
                 return 1;
             }
             //Winning Condition For Second Row
-            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            else if (boardSquares[4] == boardSquares[5] && boardSquares[5] == boardSquares[6])
             {
                 return 1;
             }
             //Winning Condition For Third Row   
-            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            else if (boardSquares[6] == boardSquares[7] && boardSquares[7] == boardSquares[8])
             {
                 return 1;
             }
             //Winning Condition For First Column
-            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            else if (boardSquares[1] == boardSquares[4] && boardSquares[4] == boardSquares[7])
             {
                 return 1;
             }
             //Winning Condition For Second Column  
-            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            else if (boardSquares[2] == boardSquares[5] && boardSquares[5] == boardSquares[8])
             {
                 return 1;
             }
             //Winning Condition For Third Column  
-            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            else if (boardSquares[3] == boardSquares[6] && boardSquares[6] == boardSquares[9])
             {
                 return 1;
             }
-            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            else if (boardSquares[1] == boardSquares[5] && boardSquares[5] == boardSquares[9])
             {
                 return 1;
             }
-            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            else if (boardSquares[3] == boardSquares[5] && boardSquares[5] == boardSquares[7])
             {
                 return 1;
             }
             // If all the cells or values filled with X or O then any player has won the match  
-            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            else if (boardSquares[1] != '1' && boardSquares[2] != '2' && boardSquares[3] != '3' && boardSquares[4] != '4' && boardSquares[5] != '5' && boardSquares[6] != '6' && boardSquares[7] != '7' && boardSquares[8] != '8' && boardSquares[9] != '9')
             {
                 return -1;
             }
