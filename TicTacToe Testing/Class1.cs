f017e3f2f0781920f71e0f8eb8df94b41a257c01
Art Weiss <tuf40221@temple.edu>
2018-10-23 19:30:38 -0400

    finished code and tests

--- TicTacToe Testing/Class1.cs
+++ TicTacToe Testing/Class1.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace TicTacToe_Testing
 {
-    class Class1
+    public class Class1
     {
+        public string[,] board;
+        public string p1;
+        public string p2;
+        public string currentPlayer;
+
+        public void createBoard()
+        {
+            board = new string[3, 3];
+            p1 = "X";
+            p2 = "O";
+            currentPlayer = p1;
+        }
+        public String getCurrentPlayer()
+        {
+            return currentPlayer;
+        }
+        public String ChangePlayer()
+        {
+            if (currentPlayer.Equals("X"))
+            {
+                currentPlayer = "O";
+            }
+            else
+            {
+                currentPlayer = "X";
+            }
+            return currentPlayer;
+            //(currentPlayer.Equals("X")) ? currentPlayer = "O" : currentPlayer = "X";
+        }
+        public void makeMove(int x, int y, string player)
+        {
+            if (!spaceInUse(x, y))
+            {
+                board[x, y] = player;
+                ChangePlayer();
+            }
+        }
+        public bool spaceInUse(int x, int y)
+        {
+            if (board[x, y] == "X" || board[x, y] == "O")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
