e0389b61bacf168114424cac8e72a3cc38f6f6cf
Collypso <collypso@gmail.com>
2018-10-23 17:09:45 -0400

    initial commit

---
+++ TicTacToe Testing/Program.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TicTacToe_Testing
+{
+    class Program
+    {
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1;
+        static int choice;
+        static int winFlag = 0;
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("p1: X   p2: O");
+                Console.Write("\n");
+
+                if (player % 2 == 0)
+                {
+                    Console.WriteLine("p2 turn");
+                }
+                else
+                {
+                    Console.WriteLine("p1 turn");
+                }
+                Console.WriteLine("\n");
+                CreateBoard();
+                choice = int.Parse(Console.ReadLine());
+
+                if (!SpaceInUse())
+                {
+                    if (player % 2 == 0)
+                    {
+                        arr[choice] = 'O';
+                        player++;
+                    }
+                    else
+                    {
+                        arr[choice] = 'X';
+                        player++;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("The row {0} is already marked with {1}", choice, arr[choice]);
+                    Console.WriteLine("\n");
+                    Console.WriteLine("Wait, board is loading again.....");
+                    Thread.Sleep(2000);
+                }
+
+                winFlag = CheckWin();
+
+            } while (winFlag == 0);
+
+            Console.Clear();
+            CreateBoard();
+
+            if (winFlag == 1)
+            {
+                Console.WriteLine("Player {0} has won", (player % 2) + 1);
+            }
+            else
+            {
+                Console.WriteLine("Draw");
+            }
+            Console.ReadLine();
+        }
+
+        public static void CreateBoard()
+        {
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+            Console.WriteLine("_____|_____|_____ ");
+            Console.WriteLine("     |     |      ");
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+            Console.WriteLine("     |     |      ");
+        }
+
+        public static bool SpaceInUse()
+        {
+            if (arr[choice] != 'X' && arr[choice] != 'O')
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private static int CheckWin()
+        {
+            //Winning Condition For First Row   
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Row
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Row   
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+            //Winning Condition For First Column
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Column  
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Column  
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+            // If all the cells or values filled with X or O then any player has won the match  
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
